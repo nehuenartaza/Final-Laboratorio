48b45ded5d3cc43fcd94f529ae4f74d60c9037a5
nehuenartaza <nehuenartaza@gmail.com>
2023-06-20 20:40:35 -0300

    falta poco

--- SpaceSoftware/main.c
+++ SpaceSoftware/main.c
@@ -40,8 +40,7 @@ typedef struct {
 
 int main()
 {
-	int opcion = 0, inicio = 1;
-	char confirmador = NULL;
+	int opcion = 0, inicio = 1, search_astronauta = 0, search_nave = 0, search_mision = 0, posicion = NULL;
 	printf ( "Para entrar al menu ingrese 1\n" );
 	scanf ( "%d", &inicio );
 	while ( inicio == 1 ) {
@@ -74,31 +73,78 @@ int main()
 			}
 			break;
         case 2:
-                drawmenu_2();
-                scanf ( "%d", &opcion );
-                switch ( opcion ) {
-                case 1:
-                    modificar_astronauta();
-                    break;
+            drawmenu_2();
+            scanf ( "%d", &opcion );
+            switch ( opcion ) {
+            case 1:
+                modificar_astronauta();
+                break;
 
-                case 2:
-                    modificar_nave();
-                    break;
+            case 2:
+                modificar_nave();
+                break;
 
-                case 3:
-                    modificar_mision();
-                }
+            case 3:
+                modificar_mision();
+                break;
+
+            case 4:
+                system("cls");
+                break;
+            }
             break;
+        case 3:
+            drawmenu_3();
+            scanf ( "%d", &opcion );
+            switch ( opcion ) {
+            case 1:
+                system("cls");
+                printf ( "Ingrese ID del astronauta: \n" );
+                scanf ( "%d", &search_astronauta );
+                posicion = consultar_astronauta(search_astronauta);
+                if ( posicion != NULL ) {
+                    mostrar_al_astronauta(posicion);
+                } else {
+                        printf ( "El astronauta no existe\n" );
+                    }
+                break;
 
+            case 2:
+                system("cls");
+                printf ( "Ingrese ID de la nave \n" );
+                scanf ( "%d", &search_nave );
+                posicion = consultar_nave(search_nave);
+                if ( posicion != NULL ) {
+
+                } else {
+                        printf ( "La nave no existe \n" );
+                    }
+                break;
+
+            case 3:
+                system("cls");
+                printf ( "Ingrese ID de la mision \n" );
+                scanf ( "%d", &search_nave );
+                posicion = consultar_nave(search_nave);
+                if ( posicion != NULL ) {
+
+                } else {
+                        printf ( "La mision no existe \n" );
+                    }
+                break;
+            }
+            break;
 		default:
 			printf ( "opcion inexistente\n" );
+			break;
 		}
 		//system("cls");
+		posicion = NULL;
 	}
   	return 0;
 }
 
-void registrar_astronauta()
+void registrar_astronauta() //crear la funcion de validar que no se repitan las ID de astronautas
 {
 	STastronauta user;
 	int opcion = 1;
@@ -146,7 +192,143 @@ void guardar_astronauta(STastronauta user)
 	}
 }
 
-void registrar_nave()
+void modificar_astronauta() //funcion que verifique que no se repita la ID
+{
+    STastronauta user, aux;
+    int valor = 0, opcion = 0;
+    mostrar_astronautas();
+    printf ( "Modifique un astronauta\n" );
+    scanf ( "%d", &valor );
+    FILE *archivo = fopen(Fastronautas,"r+b");
+    if ( archivo != NULL ) {
+        fseek(archivo, sizeof(STastronauta) * valor, SEEK_SET);
+        fread(&aux, sizeof(STastronauta), 1, archivo);
+        user = aux;
+
+        printf ( "modificar nombre? 1-Si\n" );
+        scanf ( "%d", &opcion );
+        if ( opcion == 1 ) {
+            printf ( "Nuevo nombre: \n" );
+            fflush(stdin);
+            gets ( user.nombre );
+        }
+        printf ( "cambiar apodo? 1-Si\n" );
+        scanf ( "%d", &opcion );
+        if ( opcion == 1 ) {
+            printf ( "Nuevo apodo: \n" );
+            fflush(stdin);
+            gets ( user.apodo );
+        }
+        printf ( "modificar apellido? 1-Si\n" );
+        scanf ( "%d", &opcion );
+        if ( opcion == 1 ) {
+            printf ( "Nuevo apellido: \n" );
+            fflush(stdin);
+            gets ( user.apellido );
+        }
+        printf ( "modificar ID? 1-Si\n" );
+        scanf ( "%d", &opcion );
+        if ( opcion == 1 ) { //funcion que verifique que no se repita la ID
+            printf ( "Nueva ID: \n" );
+            scanf ( "%d", &user.ID );
+        }
+        printf ( "modificar edad? 1-Si\n" );
+        scanf ( "%d", &opcion );
+        if ( opcion == 1 ) {
+            printf ( "Nueva edad: \n" );
+            scanf ( "%d", &user.edad );
+        }
+        printf ( "modificar nacionalidad? 1-Si\n" );
+        scanf ( "%d", &opcion );
+        if ( opcion == 1 ) {
+            printf ( "Nueva nacionalidad: \n" );
+            fflush(stdin);
+            gets ( user.nacionalidad );
+        }
+        printf ( "modificar especialidad? 1-Si\n" );
+        scanf ( "%d", &opcion );
+        if ( opcion == 1 ) {
+            printf ( "Nueva especialidad: \n" );
+            fflush(stdin);
+            gets ( user.especialidad );
+        }
+        printf ( "modificar horas de vuelo? 1-Si\n" );
+        scanf ( "%d", &opcion );
+        if ( opcion == 1 ) {
+            printf ( "Nueva cantidad de hrs de vuelo: \n" );
+            scanf ( &user.horas_vuelo );
+        }
+        printf ( "modificar horas en la estacion espacial? 1-Si\n" );
+        scanf ( "%d", &opcion );
+        if ( opcion == 1 ) {
+            printf ( "Nueva cantidad de hrs en la estacion espacial: \n" );
+            scanf ( &user.horas_estacion_espacial );
+        }
+        printf ( "modificar cantidad de misiones hechas? 1-Si\n" );
+        scanf ( "%d", &opcion );
+        if ( opcion == 1 ) {
+            printf ( "Nueva cantidad de misiones: \n" );
+            scanf ( &user.misiones );
+        }
+        fseek(archivo, sizeof(STastronauta) * valor, SEEK_SET);
+        fwrite(&user, sizeof(STastronauta), 1, archivo);
+        fclose(archivo);
+    }
+}
+
+void mostrar_astronautas()
+{
+    STastronauta aux;
+    int i = 0;
+    FILE *archivo = fopen(Fastronautas, "rb");
+    if ( archivo != NULL ) {
+        while ( !feof(archivo)) {
+            fread(&aux, sizeof(STastronauta), 1, archivo);
+            if ( !feof(archivo)) {
+                printf ( "|%d| %s '%s' %s, Edad:%d, ID:%d, horas de vuelo:%d, horas en la estacion:%d\n", i, aux.nombre, aux.apodo, aux.apellido, aux.edad, aux.ID, aux.horas_vuelo, aux.horas_estacion_espacial );
+                printf ( "Especialidad: %s, nacionalidad: %s, misiones hechas: %d ", aux.especialidad, aux.nacionalidad, aux.misiones );
+                if ( aux.estado == 1 ) {
+                    printf ( "Astronauta activo\n" );
+                } else {
+                        printf ( "Astronauta retirado\n" );
+                    }
+            }
+            i++;
+        }
+    }
+}
+
+int consultar_astronauta(int buscador)
+{
+    STastronauta user;
+    int posicion = NULL, i = 0;
+    FILE *archivo = fopen(Fastronautas, "rb");
+    if ( archivo != NULL ) {
+        while ( !feof(archivo) && posicion == NULL ) {
+            fread (&user, sizeof(STastronauta), 1, archivo);
+            if ( !feof(archivo) && buscador == user.ID ) {
+                posicion = i;
+            }
+            i++;
+        }
+    }
+    return posicion;
+}
+
+void mostrar_al_astronauta(int posicion)
+{
+    STastronauta user;
+    FILE *archivo = fopen(Fastronautas, "rb");
+    if ( archivo != NULL ) {
+        fseek(archivo, sizeof(STastronauta) * posicion, SEEK_SET);
+        fread(&user, sizeof(STastronauta), 1, archivo);
+        printf ( "%s '%s' %s, Edad:%d, ID:%d, horas de vuelo:%d, horas en la estacion:%d\n", user.nombre, user.apodo, user.apellido, user.edad, user.ID, user.horas_vuelo, user.horas_estacion_espacial );
+        printf ( "Especialidad: %s, nacionalidad: %s, misiones hechas: %d \n", user.especialidad, user.nacionalidad, user.misiones );
+        fclose(archivo);
+    }
+}
+
+void registrar_nave() //crear la funcion que verifique que no se repitan las ID de naves
 {
 	STnave ship;
 	int opcion = 1;
@@ -182,7 +364,62 @@ void guardar_nave(STnave ship)
 	}
 }
 
-void registrar_mision()
+void modificar_nave()
+{
+
+}
+
+void mostrar_naves()
+{
+
+}
+
+int consultar_nave(int buscador)
+{
+    STnave nave;
+    int posicion = NULL, i = 0;
+    FILE *archivo = fopen(Fnaves, "rb");
+    if ( archivo != NULL ) {
+        while ( !feof(archivo) && posicion == NULL ) {
+            fread (&nave, sizeof(STnave), 1, archivo);
+            if ( !feof(archivo) && buscador == nave.ID ) {
+                posicion = i;
+            }
+            i++;
+        }
+    }
+    return posicion;
+}
+
+void mostrar_la_nave(int posicion)
+{
+    STnave nave;
+    FILE *archivo = fopen(Fnaves, "rb");
+    if ( archivo != NULL ) {
+        fseek(archivo, sizeof(STnave) * posicion, SEEK_SET);
+        fread(&nave, sizeof(STnave), 1, archivo);
+        printf ( "ID de nave:%d, cantidad de vuelos:%d, horas de vuelo:%d ", nave.ID, nave.vuelos, nave.horas_vuelo );
+        if ( nave.tipo_nave == 1 ) {
+            printf ( "Tipo nave: Starship, " );
+        } else if ( nave.tipo_nave == 2 ) {
+                printf ( "Tipo nave: Falcon 9, " );
+            } else {
+                    printf ( "Tipo nave: Falcon-Heavy, " );
+                }
+        if ( nave.estado == 0 ) {
+            printf ( "Estado: Mantenimiento\n" );
+        } else if ( nave.estado == 1 ) {
+                printf ( "Estado: lista para su uso\n" );
+            } else if ( nave.estado == 2 ) {
+                    printf ( "Estado: actualmente en uso\n" );
+                } else {
+                        printf ( "Estado: de baja" );
+                    }
+        fclose(archivo);
+    }
+}
+
+void registrar_mision() //muchas funciones por hacer
 {
 	STmision mision;
 	int opcion = 1, i = 0; //contador de tripulantes por mision
@@ -237,156 +474,11 @@ void guardar_mision(STmision mision)
 	}
 }
 
-
-
-void drawmenu_principal()
-{
-	printf ( "1- Registrar\n" );
-	printf ( "2- Modificar\n" );
-	printf ( "3- Consultar\n" );
-	printf ( "0- Cerrar programa\n" );
-	printf ( "opcion: " );
-}
-
-void drawmenu_1()
-{
-	printf ( "1 | 1- Registrar astronautas\n" );
-	printf ( "1 | 2- Registrar naves\n" );
-	printf ( "1 | 3- Registrar misiones\n" );
-	printf ( "1 | 4- Regresar al menu principal\n" );
-	printf ( "opcion: " );
-}
-
-void drawmenu_2()
-{
-    printf ( "2 | 1- Modificar astronauta\n" );
-    printf ( "2 | 2- Modificar nave\n" );
-    printf ( "2 | 3- Modificar mision\n" );
-    printf ( "2 | 4- Regresar\n" );
-    printf ( "opcion: " );
-}
-
-void modificar_astronauta()
-{
-    STastronauta user, aux;
-    int valor = 0, opcion = 0;
-    mostrar_astronauta();
-    printf ( "Modifique un astronauta\n" );
-    scanf ( "%d", &valor );
-    FILE *archivo = fopen(Fastronautas,"r+b");
-    if ( archivo != NULL ) {
-        fseek(archivo, sizeof(STastronauta) * valor, SEEK_SET);
-        fread(&aux, sizeof(STastronauta), 1, archivo);
-        user = aux;
-
-        printf ( "modificar nombre? 1-Si\n" );
-        scanf ( "%d", &opcion );
-        if ( opcion == 1 ) {
-            printf ( "Nuevo nombre: \n" );
-            fflush(stdin);
-            gets ( user.nombre );
-        }
-        printf ( "cambiar apodo? 1-Si\n" );
-        scanf ( "%d", &opcion );
-        if ( opcion == 1 ) {
-            printf ( "Nuevo apodo: \n" );
-            fflush(stdin);
-            gets ( user.apodo );
-        }
-        printf ( "modificar apellido? 1-Si\n" );
-        scanf ( "%d", &opcion );
-        if ( opcion == 1 ) {
-            printf ( "Nuevo apellido: \n" );
-            fflush(stdin);
-            gets ( user.apellido );
-        }
-        printf ( "modificar ID? 1-Si\n" );
-        scanf ( "%d", &opcion );
-        if ( opcion == 1 ) {
-            printf ( "Nueva ID: \n" );
-            scanf ( "%d", &user.ID );
-        }
-        printf ( "modificar edad? 1-Si\n" );
-        scanf ( "%d", &opcion );
-        if ( opcion == 1 ) {
-            printf ( "Nueva edad: \n" );
-            scanf ( "%d", &user.edad );
-        }
-        printf ( "modificar nacionalidad? 1-Si\n" );
-        scanf ( "%d", &opcion );
-        if ( opcion == 1 ) {
-            printf ( "Nueva nacionalidad: \n" );
-            fflush(stdin);
-            gets ( user.nacionalidad );
-        }
-        printf ( "modificar especialidad? 1-Si\n" );
-        scanf ( "%d", &opcion );
-        if ( opcion == 1 ) {
-            printf ( "Nueva especialidad: \n" );
-            fflush(stdin);
-            gets ( user.especialidad );
-        }
-        printf ( "modificar horas de vuelo? 1-Si\n" );
-        scanf ( "%d", &opcion );
-        if ( opcion == 1 ) {
-            printf ( "Nueva cantidad de hrs de vuelo: \n" );
-            scanf ( &user.horas_vuelo );
-        }
-        printf ( "modificar horas en la estacion espacial? 1-Si\n" );
-        scanf ( "%d", &opcion );
-        if ( opcion == 1 ) {
-            printf ( "Nueva cantidad de hrs en la estacion espacial: \n" );
-            scanf ( &user.horas_estacion_espacial );
-        }
-        printf ( "modificar cantidad de misiones hechas? 1-Si\n" );
-        scanf ( "%d", &opcion );
-        if ( opcion == 1 ) {
-            printf ( "Nueva cantidad de misiones: \n" );
-            scanf ( &user.misiones );
-        }
-        fseek(archivo, sizeof(STastronauta) * valor, SEEK_SET);
-        fwrite(&user, sizeof(STastronauta), 1, archivo);
-        fclose(archivo);
-    }
-}
-
-void mostrar_astronauta()
-{
-    STastronauta aux;
-    int i = 0;
-    FILE *archivo = fopen(Fastronautas, "rb");
-    if ( archivo != NULL ) {
-        while ( !feof(archivo)) {
-            fread(&aux, sizeof(STastronauta), 1, archivo);
-            if ( !feof(archivo)) {
-                printf ( "|%d| %s '%s' %s, Edad:%d, ID:%d, horas de vuelo:%d, horas en la estacion:%d\n", i, aux.nombre, aux.apodo, aux.apellido, aux.edad, aux.ID, aux.horas_vuelo, aux.horas_estacion_espacial );
-                printf ( "Especialidad: %s, nacionalidad: %s, misiones hechas: %d ", aux.especialidad, aux.nacionalidad, aux.misiones );
-                if ( aux.estado == 1 ) {
-                    printf ( "Astronauta activo\n" );
-                } else {
-                        printf ( "Astronauta retirado\n" );
-                    }
-            }
-            i++;
-        }
-    }
-}
-
-void modificar_nave()
-{
-
-}
-
-void mostrar_nave()
-{
-
-}
-
-void modificar_mision()
+void modificar_mision() //funcion que verifique si existe el astronauta
 {
     STmision mision, aux;
     int valor = 0, opcion = 0, i = 0;
-    mostrar_mision();
+    mostrar_misiones();
     printf ( "Modifique una mision\n" );
     scanf ( "%d", &valor );
     FILE *archivo = fopen(Fmisiones,"r+b");
@@ -429,7 +521,7 @@ void modificar_mision()
         printf ( "Modificar tripulantes? 1-Si\n" );
         scanf ( "%d", &opcion );
         if ( opcion == 1 ) {
-            mostrar_astronauta();
+            mostrar_astronautas();
             printf ( "Seleccione los tripulantes por sus IDs:\n" );
             for ( i = 0; i < dimInt && opcion == 1; i++ ) {
                 scanf ( "%d", &mision.tripulantes[i] );
@@ -445,7 +537,7 @@ void modificar_mision()
     }
 }
 
-void mostrar_mision()
+void mostrar_misiones()
 {
     STmision mision;
     int i = 0, j = 0;
@@ -488,3 +580,41 @@ void mostrar_mision()
     }
 }
 
+int consultar_mision(int buscador)
+{
+    int posicion = NULL, i = 0;
+    return posicion;
+}
+
+void drawmenu_principal()
+{
+	printf ( "1- Registrar\n" );
+	printf ( "2- Modificar\n" );
+	printf ( "3- Consultar\n" );
+	printf ( "0- Cerrar programa\n" );
+	printf ( "opcion: " );
+}
+void drawmenu_1()
+{
+	printf ( "1 | 1- Registrar astronautas\n" );
+	printf ( "1 | 2- Registrar naves\n" );
+	printf ( "1 | 3- Registrar misiones\n" );
+	printf ( "1 | 4- Regresar al menu principal\n" );
+	printf ( "opcion: " );
+}
+void drawmenu_2()
+{
+    printf ( "2 | 1- Modificar astronauta\n" );
+    printf ( "2 | 2- Modificar nave\n" );
+    printf ( "2 | 3- Modificar mision\n" );
+    printf ( "2 | 4- Regresar\n" );
+    printf ( "opcion: " );
+}
+void drawmenu_3()
+{
+    printf ( "3 | 1- Consultar astronautas por ID\n" );
+    printf ( "3 | 2- Consultar naves por ID\n" );
+    printf ( "3 | 3- Consultar misiones por ID\n" );
+    printf ( "3 | 4- Regresar\n" );
+    printf ( "opcion: " );
+}
